b41ae07d7914aa312832c8adf16670e0f56380b3
alexisf <alexis.fabien45@gmail.com>
2026-01-13 13:33:32 +0100

    TP5

--- TP5/Player.js
+++ TP5/Player.js
@@ -1,92 +1,160 @@
 class Player {
   constructor(pseudo, skin, positionx, positiony) {
+    
     this.pseudo = pseudo;
     this.skin = skin;
     this.positionx = positionx;
     this.positiony = positiony;
 
-    this.hp = 10;
-    this.hpMax = 100
-    this.attack = 20;
-    this.level = 1;
-    this.speed = 1;
-    this.cooldown = 0;
-    this.cooldownMax = 1;
-    this.regen = 1;
-    this.isAlive = true;
-    this.defend = 1
+    
+    this.isDying = false;
+    this.isWalking = false;
+    this.isAttacking = false;
+    
+    
+    this.walkSpriteDuration = 2;   
+    this.WalkSpriteIndex = 0;     
+    this.WalkSpritNumber = 9;     
+    this.currentWalkSpriteStep = 0;
+
+    
+    this.attackSpritDuration = 2;
+    this.attackSpritIndex = 0;
+    this.attackSpritNumber = 6;
+    this.currentAttackPlayerSprite = 0;
+
+    
+    this.DyingSpritDuration = 2;
+    this.DyingSpritIndex = 0;
+    this.DyingSpritNumber = 6;
+    this.currentDyingPlayerSprite = 0;
   }
-  update(updateData){
-    this.hp = hp;
-    this.hpMax = hpMax
-    this.attack = attack;
-    this.level = level;
-    this.speed = speed;
-    this.cooldown = cooldown;
-    this.cooldownMax = cooldownMax;
-    this.regen = regen;
-    this.isAlive = true;
+
+  
+  update(updateData) {
+    this.hp = updateData.hp;
+    this.hpMax = updateData.hpMax;
+    this.attack = updateData.attack;
+    this.level = updateData.level;
+    this.speed = updateData.speed;
+    this.cooldown = updateData.cooldown;
+    this.cooldownMax = updateData.cooldownMax;
+    this.heal = updateData.heal;
+    this.isDying = updateData.isAlive;
+    this.isWalking = updateData.isWalking;
+    this.isattacking = updateData.isAttacking;
+    this.walkSpriteDuration= updateData.walkSpriteDuration;
+    this.WalkSpriteIndex = updateData.WalkSpriteIndex;
+    this.WalkSpritNumber = updateData.WalkSpritNumber;
+    this.currentWalkSpriteStep = updateData.currentWalkSpriteStep
+    this.currentAttackPlayerSprite
+    this.attackSpritDuration
+    this.attackSpritIndex
+    this.attackSpritNumber
   }
 
-  updateTakePlayer(updateData) {
-    if (!this.isAlive) {
-      console.log(
-        `${this.pseudo} est déjà KO et ne peut plus subir de dégâts.`
-      );
-      return;
-    }
 
-    this.hp -= updateData;
-    console.log(
-      `${this.pseudo} subit -${updateData} dégâts. PV restants : ${this.hp}`
-    );
+  animate() {
+    let animationFinished = false;
 
-    if (this.hp <= 0) {
-      this.hp = 0;
-      this.isAlive = false;
-      console.log(`--- ${this.pseudo} est mort au combat ! ---`);
+    // Logique des états
+    if (this.isDying) {
+      this.currentDyingPlayerSprite++;
+      if (this.currentDyingPlayerSprite >= this.DyingSpritDuration) {
+        this.currentDyingPlayerSprite = 0;
+        this.DyingSpritIndex++;
+      }
+      if (this.DyingSpritIndex >= this.DyingSpritNumber) {
+        this.DyingSpritIndex = this.DyingSpritNumber - 1; 
+        animationFinished = true; 
+      }
+    } 
+    // Logique attaque
+    else if (this.isAttacking) {
+      this.currentAttackPlayerSprite++;
+      if (this.currentAttackPlayerSprite >= this.attackSpritDuration) {
+        this.currentAttackPlayerSprite = 0;
+        this.attackSpritIndex++;
+      }
+      if (this.attackSpritIndex >= this.attackSpritNumber) {
+        this.attackSpritIndex = 0; 
+      }
+    } 
+    // Logique marche
+    else if (this.isWalking) {
+      this.currentWalkSpriteStep++;
+      if (this.currentWalkSpriteStep >= this.walkSpriteDuration) {
+        this.currentWalkSpriteStep = 0;
+        this.WalkSpriteIndex++;
+      }
+      if (this.WalkSpriteIndex >= this.WalkSpritNumber) {
+        this.WalkSpriteIndex = 0; 
+      }
     }
-
-    if (this.isAlive) {
-      this.level += 1;
-      this.hp += 5;
-      this.attack = this.attack*(1.2);
-      console.log(
-        `Félicitations ! ${this.pseudo} passe au niveau ${this.level}. PV augmentés ! `,
-      );
-      console.log("hp", player1.hp)
-console.log("attack", player1.attack.toFixed(2))
-console.log("level", player1.level)
-console.log("speed", player1.speed)
-console.log("regen", player1.regen)
-console.log("isalive", player1.isAlive)
-console.log("defend", player1.defend)
-    }
+    return animationFinished;
   }
 }
 
+//  INITIALISATION ET SIMULATION 
 
+const alex = new Player("alex", 42, 1, [0, 0]);
+let tempsRestantAction = 0; 
+
+for (let i = 0; i < 30; i++) {
   
-console.log("=== Test de la classe Player ===");
   
-const player1 = new Player("AFB", "Armure en or");
+  if (tempsRestantAction <= 0 && !alex.isDying) {
+    
+    // Réinitialisation de tous les états
+    alex.isWalking = false;
+    alex.isAttacking = false;
+    alex.isDying = false;
+    
+    alex.WalkSpriteIndex = 0;
+    alex.currentWalkSpriteStep = 0;
+    alex.attackSpritIndex = 0;
+    alex.currentAttackPlayerSprite = 0;
 
+    // Choix aléatoire
+    const choix = Math.floor(Math.random() * 3); 
+    if (choix === 0) alex.isWalking = true;
+    else if (choix === 1) alex.isAttacking = true;
+    else if (choix === 2) alex.isDying = true;
+    
+    // Définit la durée de l'action
+    tempsRestantAction = Math.floor(Math.random() * 5) + 4;
 
-console.log("Joueur créé :", player1);
+    console.log("===========================================");
+    console.log(` changement d'état : ${alex.isDying ? 'mort' : alex.isAttacking ? 'attaque' : 'marche'}`);
+    console.log("===========================================");
+  }
 
-player1.updateTakePlayer(player1.defend);
+  tempsRestantAction--;
+  const mortTerminee = alex.animate(); 
 
-player1.updateTakePlayer(player1.defend);
+  // Affichage des données
+  console.log("----------------MARCHE----------------")
+  console.log("isWalking", alex.isWalking);
+  console.log("WalkSpriteIndex", alex.WalkSpriteIndex);
+  console.log("currentWalkSpriteStep", alex.currentWalkSpriteStep)
 
-player1.updateTakePlayer(player1.defend);
+  console.log("----------------ATTAQUE----------------")
+  console.log("isattack", alex.isAttacking);
+  console.log("attackindex", alex.attackSpritIndex);
+  console.log("cuurentattack", alex.currentAttackPlayerSprite)
 
-player1.updateTakePlayer(player1.cooldownMax);
+  console.log("----------------MORT----------------")
+  console.log("isdying", alex.isDying);
+  console.log("dyingindex", alex.DyingSpritIndex);
+  console.log("currentdying", alex.currentDyingPlayerSprite)
 
-console.log("=== Test de la classe Player ===");
+  if (alex.isDying === true) {
+    console.log("vous etes Mort")
+  }
 
-console.log("position", player1.positionx, "______", player1.positiony)
-
-
-console.log("État final :", player1);
-
-console.log("cooldown", player1.cooldownMax)
+  // Condition de sortie
+  if (alex.isDying && mortTerminee) {
+    console.log("\nFin du jeu");
+    break;
+  }
+}
